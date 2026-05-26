22a4118e2c3aba2509a51f68e52259bfa60e1674
vivek-ottimate <vivek.tiwari@ottimate.com>
2026-05-26 13:48:47 +0530

    Add end-to-end hello world: Django API + Angular HttpClient
    - Django: api app with GET /api/hello/ returning JSON
    - Angular: ApiService with HttpClient calling the endpoint
    - AppComponent displays backend message or connection error
    - HttpClientModule added to AppModule

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  private apiUrl = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  getHello(): Observable<{ message: string }> {
+    return this.http.get<{ message: string }>(`${this.apiUrl}/hello/`);
+  }
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  message = '';
+  error = '';
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.api.getHello().subscribe({
+      next: (data) => this.message = data.message,
+      error: () => this.error = 'Could not connect to Django backend.'
+    });
+  }
 }
